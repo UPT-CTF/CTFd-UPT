17cdcb58912904a02618e5ec0cb129559698035a
thek0d3r <horjarazvan@gmail.com>
2025-09-09 02:25:48 +0300

    Updated views.py for the theme to work

--- CTFd/views.py
+++ CTFd/views.py
@@ -361,6 +361,11 @@ def static_html(route):
         if page.auth_required and authed() is False:
             return redirect(url_for("auth.login", next=request.full_path))
 
+        if route == "index":
+            return render_template(
+                ["index.html", "page.html"], content=page.html, title=page.title
+            )
+
         return render_template("page.html", content=page.html, title=page.title)
 
 
